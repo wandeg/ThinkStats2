eec0478f8d3e16b72bd83ac131864f7ac03eec45
James Maringa <james.maringa@gro-intelligence.com>
2016-04-05 23:02:13 +0300

    Chap 1 ex 1 num 2

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -5,8 +5,6 @@ Copyright 2014 Allen B. Downey
 License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
-from __future__ import print_function
-
 import numpy as np
 import sys
 
@@ -14,11 +12,39 @@ import nsfg
 import thinkstats2
 
 
+def ReadFemPreg(dct_file='2002FemPreg.dct',
+                dat_file='2002FemPreg.dat.gz'):
+    """Reads the NSFG pregnancy data.
+
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip')
+    nsfg.CleanFemPreg(df)
+    return df
+
+def NumPregs(df):
+	mapper = nsfg.MakePregMap(df)
+	cs = {}
+	for k,v in mapper.iteritems():
+		cs[len(mapper[k])] = cs.get(len(mapper[k]),0) +1
+
+	print cs
+
+	return df.pregnum.value_counts()
+
+
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+    df = ReadFemPreg()
+    print NumPregs(df)
+    
     print('%s: All tests passed.' % script)
 
 
